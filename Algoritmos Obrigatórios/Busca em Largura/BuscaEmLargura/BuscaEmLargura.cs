2488143ecc79e9957d09b0798f069d8ae772d8aa
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-11-26 17:59:14 -0300

    adicionando atributos

--- Algoritmos Obrigatórios/Busca em Largura/BuscaEmLargura/BuscaEmLargura.cs
+++ Algoritmos Obrigatórios/Busca em Largura/BuscaEmLargura/BuscaEmLargura.cs
@@ -1,3 +1,20 @@
 public class BuscaEmLargura{
-    
+    // matriz de adjacência de um grafo esparso
+    public int[,] grafo_esparso = new int[10,10]{
+        {0,1,1,1,1,0,0,0,0,0},
+        {1,0,0,0,1,1,0,0,0,0},
+        {1,0,0,1,0,1,0,0,0,0},
+        {1,0,1,0,0,1,0,0,1,0},
+        {1,1,0,0,0,1,1,1,0,0},
+        {0,1,1,1,1,0,1,0,1,0},
+        {0,0,0,0,1,1,0,1,1,1},
+        {0,0,0,0,1,0,1,0,0,1},
+        {0,0,0,1,0,1,1,0,0,1},
+        {0,0,0,0,0,0,1,1,1,0}
+    };
+    // fila que armazena os nós a serem visitados
+    public Queue<int> fila_execucao = new Queue<int>();
+    public int [] nos_visitados = new int[10];
+    public int inicio = 0;
+    public int final = 8;
 }
