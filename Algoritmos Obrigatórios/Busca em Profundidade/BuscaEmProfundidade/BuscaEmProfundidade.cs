cafbb558bb827b63231cd8178c3b0fddea1b1cd4
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-12-10 18:29:55 -0300

    Definindo grafo denso

--- Algoritmos Obrigatórios/Busca em Profundidade/BuscaEmProfundidade/BuscaEmProfundidade.cs
+++ Algoritmos Obrigatórios/Busca em Profundidade/BuscaEmProfundidade/BuscaEmProfundidade.cs
@@ -12,6 +12,19 @@ public class BuscaEmProfundidade{
         {0,0,0,1,0,1,1,0,0,1},
         {0,0,0,0,0,0,1,1,1,0}
     };
+    // matriz de adjacência de um grafo denso
+    public int[,] grafo_denso = new int[10,10]{
+        {0,1,1,1,0,1,1,1,1,1},
+        {1,0,1,1,1,1,0,1,1,1},
+        {1,1,0,1,1,1,1,0,1,1},
+        {1,1,1,0,1,1,1,1,0,1},
+        {0,1,1,1,0,1,1,1,1,1},
+        {1,1,1,1,1,0,1,1,1,0},
+        {1,0,1,1,1,1,0,1,1,1},
+        {1,1,0,1,1,1,1,0,1,1},
+        {1,1,1,0,1,1,1,1,0,1},
+        {1,1,1,1,1,0,1,1,1,0}
+    };
     // pilha que armazena os nós a serem visitados
     public Stack<int> pilha_execucao = new Stack<int>();
     public int[] nos_visitados = new int[10];
@@ -27,7 +40,7 @@ public class BuscaEmProfundidade{
         Console.ForegroundColor = ConsoleColor.Magenta; // imprime uma mensagem colorida
         Console.WriteLine("\nExecutando busca em profundidade para grafo denso:");
         Console.ResetColor(); // restaura a cor padrão
-        // comecar_busca(grafo_denso, inicio, final);
+        comecar_busca(grafo_denso, inicio, final);
     }
 
     public void comecar_busca(int[,] grafo, int inicio, int fim){
@@ -40,12 +53,12 @@ public class BuscaEmProfundidade{
         while(true){
             if(!pilha_execucao.Contains(no_atual)){ // se o nó atual não está na pilha
                 pilha_execucao.Push(no_atual); // adiciona o nó à pilha
-                Console.WriteLine($"O nó {no_atual} foi adicionado à pilha.");
+                Console.WriteLine($"O nó {no_atual + 1} foi adicionado à pilha.");
             }
 
             if(no_atual == no_final){ // se o nó atual for o nó que está sendo buscado
                 pilha_execucao.Pop(); // remove o nó da pilha
-                Console.WriteLine($"O nó {no_final} foi encontrado!");
+                Console.WriteLine($"O nó {no_final + 1} foi encontrado!");
                 break; // termina a execução
             }
             else{
@@ -55,7 +68,7 @@ public class BuscaEmProfundidade{
                 pilha_execucao.Pop();
                 // o nó é marcado no vetor que armazena os nós que já foram visitados
                 nos_visitados[aux] = 1;
-                Console.WriteLine($"O nó {no_atual} foi visitado.");
+                Console.WriteLine($"O nó {no_atual + 1} foi visitado.");
 
                 for(int i=0; i<10; i++){
                     // 1) se tiver uma aresta entre o nó atual e qualquer outro nó do grafo
@@ -63,7 +76,8 @@ public class BuscaEmProfundidade{
                     // 3) se esse nó não ser um dos nós que foram visitados
                     if(grafo[aux, i] == 1 && !pilha_execucao.Contains(i) && nos_visitados[i] == 0){
                         pilha_execucao.Push(i); // adiciona o nó ao topo da pilha
-                        Console.WriteLine($"O nó {i} foi adicionado à pilha.");
+                        Console.WriteLine($"O nó {i+1} foi adicionado à pilha.");
+    
                     }
                 }
                 no_atual = pilha_execucao.Peek(); // o nó atual é o nó no topo da pilha
